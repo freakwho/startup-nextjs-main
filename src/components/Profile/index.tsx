e476ba7e1cb2e0c9debe3a3fec10efa53e74c961
freakwho <freakwho123@gmail.com>
2025-08-24 18:57:00 +0700

    Final

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,13 +2,13 @@ import Data from "../Team/interface";
 import axios from "axios";
 
 const Profile = async () => {
-  const {data} = await axios.get(process.env.NEXT_PUBLIC_TEAM_API);
+  const { data } = await axios.get(process.env.NEXT_PUBLIC_TEAM_API);
   const List = ({ text }) => (
     <p className="mb-5 items-center text-lg font-medium text-body-color">
       {text}
     </p>
   );
-    return (
+  return (
     <>
       <section className="relative overflow-hidden">
         <div className="container">
@@ -19,10 +19,10 @@ const Profile = async () => {
                 Who We Are
               </h1>
               <p className="text-2xl">
-                Actually, next driver is an archive where the drivers for one type devices are stored. 
-                For instance, for sound cards. They are sorted within the archive on producers, and release dates. 
-                Next Driver is software that saves your time and nerves you spend to install and update the drivers. 
-                Next Driver scans a computer by itself, detects which drivers exactly are required, and installs them. 
+                Actually, next driver is an archive where the drivers for one type devices are stored.
+                For instance, for sound cards. They are sorted within the archive on producers, and release dates.
+                Next Driver is software that saves your time and nerves you spend to install and update the drivers.
+                Next Driver scans a computer by itself, detects which drivers exactly are required, and installs them.
                 Next Driver installs official drivers from the producers, and has one of the world largest driver databases.
               </p>
             </div>
@@ -37,7 +37,7 @@ const Profile = async () => {
               <div className="mb-10 w-1/3 px-2">
                 <div className="flex justify-around">
                   <div className="mb-2 mr-2 flex h-[70px] w-[70px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
-                    <img src={data.results[1].picture.large}/>
+                    <img src="/images/profil/Arthur.jpeg" />
                   </div>
                   <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
                     Artur Kovyakuz <br />
@@ -45,7 +45,7 @@ const Profile = async () => {
                   </h3>
                 </div>
                 <p>Expertise: Software Driver Strategy, Business Development, Software Engineering. <br /> <br />
-                  Artur founded Next Driver in 2020, driven by a mission to help Indonesian 
+                  Artur founded Next Driver in 2020, driven by a mission to help Indonesian
                   businesses embrace Software Driver technology.</p>
               </div>
 
@@ -55,7 +55,7 @@ const Profile = async () => {
               <div className="mb-10 w-1/3 px-2">
                 <div className="flex justify-around">
                   <div className="mb-2 mr-2 flex h-[70px] w-[70px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
-                    <img src={data.results[2].picture.large}/>
+                    <img src="/images/profil/Aria.jpeg" />
                   </div>
                   <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
                     Aria <br />
@@ -63,7 +63,7 @@ const Profile = async () => {
                   </h3>
                 </div>
                 <p>Expertise: Machine Learning, Deep Learning, Software Development. <br /><br />
-                  Aria leads Next Driver innovation to ensure 
+                  Aria leads Next Driver innovation to ensure
                   that our software driver solutions remain cutting-edge.</p>
               </div>
             </div>
@@ -72,11 +72,11 @@ const Profile = async () => {
           <div>
             <h1 className="flex justify-start mb-4 text-6xl font-bold text-black dark:text-white">Culture</h1>
             <p className="font-medium leading-relaxed text-white text-2xl sm:leading-relaxed">
-              At Next Driver, we believe in fostering a work culture that is collaborative, innovative, and inclusive. 
+              At Next Driver, we believe in fostering a work culture that is collaborative, innovative, and inclusive.
               We encourage every member of our team to challenge conventional thinking and approach problems with a fresh perspective.
             </p>
           </div>
-          
+
         </div>
       </section>
     </>
